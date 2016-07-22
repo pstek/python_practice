48d34c07eeada90ac627ca61a74ef6cd1b30c807
destinybeom <hyunssuni001@gmail.com>
2016-07-22 18:01:03 +0900

    Revert "First Commit"
    This reverts commit ec1e4d483078bab592191215e06ea46152fc8816.

--- 5-1/exam.py
+++
@@ -1,8 +0,0 @@
- #-*- coding: utf-8 -*-
-class Service:
-	secret="영구 배꼽은 2개"
-	def __init__(self,name):
-		self.name=name
-	def sum(self,a,b):
-		result=a+b
-		print("%s님, %s + %s = %s 입니다." % (self.name,a,b,result))
